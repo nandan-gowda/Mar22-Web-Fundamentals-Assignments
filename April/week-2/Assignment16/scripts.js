165d8e4bb860252dd3693f1b57bea36dcd050945
nandan <nanduu1111@gmail.com>
2022-04-12 09:06:06 +0530

    testing

--- April/week-2/Assignment16/scripts.js
+++ April/week-2/Assignment16/scripts.js
@@ -1,3 +1,13 @@
+const image1= document.querySelector('img')
+
+image1.addEventListener('mouseover', beforehoverfn)
+function beforehoverfn(){
+    image1.classList= 'beforehover'
+}
 
 
 
+image1.addEventListener('mouseout', afterhoverfn)
+function afterhoverfn(){
+    image1.classList= 'afterhover'
+}
