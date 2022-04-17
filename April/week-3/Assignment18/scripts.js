53f5c96c661a51d7a55447a757689547bbde17d9
nandan <nanduu1111@gmail.com>
2022-04-17 16:33:00 +0530

    assignment

--- April/week-3/Assignment18/scripts.js
+++ April/week-3/Assignment18/scripts.js
@@ -5,10 +5,29 @@ const students = [
   { name: "john 3", score: 65 },
   { name: "john 4", score: 85 },
 ];
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+table.appendChild(thead);
+table.appendChild(tbody);
+document.getElementById('tableBody').appendChild(table);
+
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "name";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "score";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+thead.appendChild(row_1);
+
+
+let sumscore = 0;
 
 function passedStudents(studentList) {
   const finalArr = [];
-  let sumscore = 0;
   for (let i = 0; i < studentList.length; i++) {
     if (studentList[i].score < 50) {
       const obj = {
@@ -16,17 +35,34 @@ function passedStudents(studentList) {
         score: studentList[i].score + 10,
       };
       finalArr.push(obj);
-    } else {
+      
+     
+
+   
+
+} else {
       finalArr.push(studentList[i]);
     }
   }
   console.log(finalArr);
   for (let i = 0; i < finalArr.length; i++) {
-    if (finalArr[i].score > 50) {
+    if (finalArr[i].score >= 50) {
       console.log(finalArr[i].name , finalArr[i].score,  " has passed");
+      sumscore= sumscore+finalArr[i].score
+      let row_2 = document.createElement('tr');
+      let row_2_data_1 = document.createElement('td');
+      row_2_data_1.innerHTML = studentList[i].name;
+      let row_2_data_2 = document.createElement('td');
+      row_2_data_2.innerHTML = studentList[i].score;
+      row_2.appendChild(row_2_data_1);
+      row_2.appendChild(row_2_data_2);
+      tbody.appendChild(row_2);
+
     } else {
       console.log(finalArr[i].name, finalArr[i].score, " failed the test");
     }
   }
 }
 console.log(passedStudents(students))
+console.log('Total Score of all the passed students is ' , sumscore)
+
